13c108c0aad4329e5fcff5941ccb18c1553c20ec
Kaehyuni <nguyeken017@gmail.com>
2026-04-22 00:30:38 -0500

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import FitnessAppMidFi from "./App"; // this is your App.jsx file
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <FitnessAppMidFi />
-  </React.StrictMode>
-);
