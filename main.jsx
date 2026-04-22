201f1be1cee9a01a2cc5e71d91f817bf404b3f26
Kaehyuni <nguyeken017@gmail.com>
2026-04-22 00:07:37 -0500

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,1 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import FitnessAppMidFi from "./App"; // this is your App.jsx file
 
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <FitnessAppMidFi />
+  </React.StrictMode>
+);
